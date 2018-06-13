826f8c665399515606d9c9a108c8fa84405f6d09
ibalagurov <balagurovi@yandex.ru>
2018-06-14 02:24:23 +0300

    add helper method

--- testlib/disk/helper.py
+++ testlib/disk/helper.py
@@ -5,3 +5,9 @@ from . import connector
 def get_disk_info_response():
     request = connector.disk(method='get')
     return http_session.send_request(request=request)
+
+
+def get_disk_info():
+    response = get_disk_info_response()
+    response.raise_for_status()
+    return response.json()
