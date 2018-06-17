fcb2752940b31141c8af70bc3ea97a048a24f38a
ibalagurov <balagurovi@yandex.ru>
2018-06-18 02:37:33 +0300

    add base tests and matchers

---
+++ testlib/check.py
@@ -0,0 +1,40 @@
+# matchers
+
+
+def response_has_status_code(response, status_code):
+    actual_status_code = response.status_code
+    assert status_code == actual_status_code, f'''
+        Expected response status code: {status_code}
+        Actual status code: {actual_status_code}
+    '''
+
+
+def response_does_not_have_fields(response, *fields):
+    json = response.json()
+    unexpected_fields = [field for field in fields if json.get(field)]
+    assert not unexpected_fields, f'Response contains unexpected fields: {unexpected_fields}'
+
+
+def response_has_fields(response, *fields):
+    json = response.json()
+    missing_fields = [field for field in fields if json.get(field) is None]
+    assert not missing_fields, f'Response does not have expected fields: {missing_fields}'
+
+
+def response_has_only_fields(response, *fields):
+    json = response.json()
+    missing_fields = [field for field in fields if json.get(field) is None]
+    assert not missing_fields, f'Response does not have expected fields: {missing_fields}'
+    unexpected_fields = [key for key in json.keys() if key not in fields]
+    assert not unexpected_fields, f'Response has unexpected fields: {unexpected_fields}'
+
+
+def response_has_field_with_value(response, field, value):
+    actual_field_value = response.json().get(field)
+    assert actual_field_value, f'Response does not have expected {field} field'
+    assert value == actual_field_value, f'''
+        Expected '{field}' field value: 
+        {value}
+        Actual '{field}' field value:
+        {actual_field_value}
+    '''
---
+++ tests/test_disk.py
@@ -0,0 +1,27 @@
+import pytest
+from testlib import check
+from testlib import disk
+
+
+def test_get_disk_info():
+    response = disk.helper.get_disk_info_response()
+    check.response_has_status_code(response, 200)
+    check.response_has_fields(
+        response, 'system_folders', 'user', 'max_file_size', 'is_paid', 'used_space'
+    )
+
+
+def test_unauthorized_user_do_not_have_access_to_disk_info():
+    response = disk.helper.get_disk_info_response(by_user='unauthorized')
+    check.response_has_status_code(response, 401)
+    check.response_has_field_with_value(response, field='error', value='UnauthorizedError')
+    check.response_does_not_have_fields(
+        response, 'system_folders', 'user', 'max_file_size', 'is_paid', 'used_space'
+    )
+
+
+@pytest.mark.parametrize('method', ['post', 'put', 'delete', 'patch'])
+def test_unexpected_methods_are_not_allowed(method):
+    response = disk.helper.custom_disk_response(method=method, by_user='authorized')
+    check.response_has_status_code(response, 405)
+    check.response_has_field_with_value(response, field='error', value='MethodNotAllowedError')
