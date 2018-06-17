6b23df2e117e5715188217ecaa03b5239119b760
ibalagurov <balagurovi@yandex.ru>
2018-06-18 02:36:10 +0300

    cosmetic

--- testlib/disk/connector.py
+++ testlib/disk/connector.py
@@ -1,9 +1,10 @@
 from requests import Request
+from functools import partial
 
 import config
 
 DISK_URL = config.routing.BASE_URL + '/disk'
 
+disk = partial(Request, url=DISK_URL)
 
-def disk(method):
-    return Request(method=method, url=DISK_URL)
+
