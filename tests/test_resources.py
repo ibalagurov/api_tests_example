38e62947001739b505b291ceac90631555d6e93f
ibalagurov <balagurovi@yandex.ru>
2018-06-19 00:26:22 +0300

    extend test for different file types

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,14 +1,20 @@
+import pytest
 from testlib import resources, operations
 from testlib import check
 from waiting import wait
 
 
-def test_upload(temp_folder):
+@pytest.mark.parametrize('link, file_type, mime_type, file_name', [
+    (
+        'https://habrastorage.org/getpro/habr/post_images/ac4/d9f/2a9/ac4d9f2a99bca3d3342b7f440f62ba3d.jpg',
+        'file', 'image/jpeg', 'test.jpg'
+    )
+])
+def test_upload(temp_folder, link, file_type, mime_type, file_name):
     """ Temporary folder is created, upload in it image, operation should be successful, file should be available and
         have expected name and mime type """
-    test_link = 'https://habrastorage.org/getpro/habr/post_images/ac4/d9f/2a9/ac4d9f2a99bca3d3342b7f440f62ba3d.jpg'
-    file_path = f'{temp_folder}/test.jpg'
-    response = resources.helper.post_upload_resource_response(params={'path': file_path, 'url': test_link})
+    file_path = f'{temp_folder}/{file_name}'
+    response = resources.helper.post_upload_resource_response(params={'path': file_path, 'url': link})
 
     check.response_has_status_code(response, 202)
     check.response_has_fields(response, 'href')
@@ -19,6 +25,6 @@ def test_upload(temp_folder):
 
     response = resources.helper.get_resources_response(params={'path': file_path})
     check.response_has_status_code(response, 200)
-    check.response_has_field_with_value(response, "type", "file")
-    check.response_has_field_with_value(response, "mime_type", "image/jpeg")
-    check.response_has_field_with_value(response, "name", "test.jpg")
+    check.response_has_field_with_value(response, "type", file_type)
+    check.response_has_field_with_value(response, "mime_type", mime_type)
+    check.response_has_field_with_value(response, "name", file_name)
